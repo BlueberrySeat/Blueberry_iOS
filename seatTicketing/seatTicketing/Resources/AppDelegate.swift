e50c91fd8ae282ea33ca5bb16815e3c17739046e
kimxwan0319 <kimxwan0319@naver.com>
2020-09-17 09:45:48 +0900

    [Update] version target down

--- seatTicketing/seatTicketing/Resources/AppDelegate.swift
+++ seatTicketing/seatTicketing/Resources/AppDelegate.swift
@@ -12,6 +12,7 @@ import FirebaseCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
